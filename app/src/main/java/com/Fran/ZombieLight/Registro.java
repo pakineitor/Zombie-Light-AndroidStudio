ddeb7d19ecd991eda5818f902d5a1813c4f6eec5
pakineitor <franreyespenia@gmail.com>
2023-08-09 00:18:19 +0200

    Versión 0.4
    Se ha modificado un pequeño error en back end.

--- app/src/main/java/com/Fran/ZombieLight/Registro.java
+++ app/src/main/java/com/Fran/ZombieLight/Registro.java
@@ -5,20 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registro extends AppCompatActivity {
-    EditText Correo, Password, Nombre, Fechatxt;            //Declaramos las variables.
+    EditText Correo, Password, Nombre;            //Declaramos las variables.
+    TextView Fechatxt;
     Button Aceptar;
+    FirebaseAuth auth;                                      // Esta variable sirve para nstanciar el registro de cada usuario con la bd de Firebase.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-                                                           //Vamos a referenciar los elementos EditText de la clase con la activity.xml-->
+                                                           // Conexión con la vista:
+                                                           // Vamos a referenciar los elementos EditText de la clase con la activity_registro.xml-->
         Correo      = findViewById(R.id.Correo);
         Password    = findViewById(R.id.Password);
         Nombre      = findViewById(R.id.Nombre);
+                                                           // Vamos a referenciar el elemento Textview de la clase con el del activity_registro.xml-->
         Fechatxt    = findViewById(R.id.Fechatxt);
-                                                            //Vamos a referenciar los elementos Button de la clase con la activity.xml-->
+                                                           // Vamos a referenciar los elementos Button de la clase con la activity_registro.xml-->
         Aceptar     = findViewById(R.id.Aceptar);
 
 
